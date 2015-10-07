e456a1db9cc7a9e841d25d8edf0fbf91b86bfa46
Feliszt <fcote@kth.se>
2015-10-07 19:53:51 +0200

    Extracts the feature of the hummed song from the frIsequence matrix computed by GetMusicGeatures

---
+++ GetFeatures.m
@@ -0,0 +1,13 @@
+function [ features ] = GetFeatures( frisequence )
+%GetFeatures Extracts the feature of the hummed song from the frIsequence
+%Matrix produced by GetMusicFeatures.
+%   TODO : - choose PattRecClasses output
+%          - choose features to extract
+%          - implement the extraction
+
+
+
+
+
+end
+
